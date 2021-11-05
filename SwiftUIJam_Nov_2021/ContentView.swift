d7837718698c99e4a53f27f49edcdf68014e6e1d
Takasur Azeem <takasurazeem@gmail.com>
2021-11-06 00:10:57 +0500

    initial commit

---
+++ SwiftUIJam_Nov_2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIJam_Nov_2021
+//
+//  Created by Takasur Azeem on 06/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
