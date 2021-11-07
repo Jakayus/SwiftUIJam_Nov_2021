3087d1990db38090e6efc9746ab55f40e65e43bc
Tallak-Lippe <tallak.lvdl@gmail.com>
2021-11-07 16:52:07 +0100

    Accounting for stupid long expressions

--- SwiftUIJam_Nov_2021/Views/DisplayView.swift
+++ SwiftUIJam_Nov_2021/Views/DisplayView.swift
@@ -19,52 +19,14 @@ struct DisplayView: View {
     @State private var animationStartTask: Task = Task{}
     var body: some View {
         HStack {
-            Text(expression)
+            //An empty expression messed with the height of the cursor
+            Text(expression != "" ? expression : " ")
                 .font(.system(size: 42, weight: .medium, design: .monospaced))
+                .lineLimit(1)
+                .minimumScaleFactor(0.3)
+                .truncationMode(.head)
                 .overlay(
-                    GeometryReader { proxy in
-                        HStack(spacing: 0){
-                        //Cursor
-                            RoundedRectangle(cornerRadius: 2)
-                                .frame(width: 4)
-                                .opacity(cursorAnimating ? cursorOpacity : 1)
-                            //Decides the position of the cursor
-                                .offset(x: proxy.size.width * CGFloat(cursorIndex)/CGFloat(expression.count))
-                                .animation(nil, value: displayPressed)
-                            //Cursor gets larger when it is moved
-                                .padding(.vertical, displayPressed ? -2 : 2)
-                                .animation(.easeInOut, value: displayPressed)
-                            //Start the cursor animation syklus.
-                                .onAppear() {
-                                    withAnimation(.easeInOut(duration: 0.5).delay(0.5).repeatForever()) {
-                                        self.cursorOpacity = 0
-                                    }
-                                }
-                            //When cursor moves, the animation timer is reset.
-                                .onChange(of: cursorIndex) {_ in
-                                    resetCursorAnimationTimer()
-                                    //Taptic feedback when the cursor changes position while dragging
-                                    if displayPressed {
-                                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                                    }
-                                }
-                                
-                            Spacer()
-                        }
-                        .contentShape(Rectangle())
-                        .gesture(
-                            //Gesture to move the cursor
-                            DragGesture(minimumDistance: 0, coordinateSpace: .named("expressionDisplay"))
-                                .onChanged({ offset in
-                                    displayPressed = true
-                                    calculateCursorIndex(proxy: proxy, offset: offset)
-                                })
-                                .onEnded({ _ in
-                                    displayPressed = false
-                                })
-                        )
-                    }
-                    .coordinateSpace(name: "expressionDisplay")
+                    cursor
                 )
             Spacer()
             if let answer = answer {
@@ -82,7 +44,64 @@ struct DisplayView: View {
 
     }
     
+    
+    //I do kind of feel bad for the people that coded the first cursors. This is complicated even with monospace characters.
+    var cursor: some View {
+        GeometryReader { proxy in
+            HStack(spacing: 0){
+            //Cursor
+                RoundedRectangle(cornerRadius: 2)
+                    .frame(width: 4)
+                    .opacity(cursorAnimating ? cursorOpacity : 1)
+                //Decides the position of the cursor
+                    .offset(x: calculateCursorOffset(proxy: proxy))
+                    .animation(nil, value: displayPressed)
+                //Cursor gets larger when it is moved
+                    .padding(.vertical, displayPressed ? -2 : 2)
+                    .animation(.easeInOut, value: displayPressed)
+                                    
+                Spacer()
+            }
+            .contentShape(Rectangle())
+            .gesture(
+                //Gesture to move the cursor
+                DragGesture(minimumDistance: 0, coordinateSpace: .named("expressionDisplay"))
+                    .onChanged({ offset in
+                        displayPressed = true
+                        calculateCursorIndex(proxy: proxy, offset: offset)
+                    })
+                    .onEnded({ _ in
+                        displayPressed = false
+                    })
+            )
+            .coordinateSpace(name: "expressionDisplay")
+        //Start the cursor animation syklus.
+            .onAppear() {
+                withAnimation(.easeInOut(duration: 0.5).delay(0.5).repeatForever()) {
+                    self.cursorOpacity = 0
+                }
+            }
+        //When cursor moves, the animation timer is reset.
+            .onChange(of: cursorIndex) {_ in
+                resetCursorAnimationTimer()
+                //Taptic feedback when the cursor changes position while dragging
+                if displayPressed {
+                    UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                }
+            }
+
+        }
+    }
+    
+    func calculateCursorOffset(proxy: GeometryProxy) -> CGFloat {
+        //If there is time to account for truncating, the code should just be put here, probably
+        //Can't divide by 0 you know.
+        let expressionLength = max(expression.count, 1)
+        return proxy.size.width * CGFloat(cursorIndex)/CGFloat(expressionLength)
+    }
+
     func calculateCursorIndex(proxy: GeometryProxy, offset: DragGesture.Value) {
+        //If there is time to account for truncating, the code should just be put here, probably here too
         let displayWidth = proxy.frame(in: .named("expressionDisplay")).width
         let ratio = offset.location.x / displayWidth
         let newCursorIndex = Int(ceil((ratio * CGFloat(expression.count)) - 0.5))
@@ -109,7 +128,7 @@ struct DisplayView: View {
 fileprivate struct Wrapper: View {
     @State var cursorIndex = 3
     var body: some View {
-        DisplayView(expression: "1+1*3-sqrt(2)", answer: "4",errorMessage: "Couldn't find operator '&'", cursorIndex: $cursorIndex)
+        DisplayView(expression: " ", answer: "4",errorMessage: "Couldn't find operator '&'", cursorIndex: $cursorIndex)
     }
 }
 
